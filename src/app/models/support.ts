1dde8d770aaf9003bdc11d360b6c1def19e39dca
KhoaHV2 <KhoaHV2@fsoft.com.vn>
2018-10-03 17:09:20 +0700

    update

---
+++ src/app/models/support.ts
@@ -0,0 +1,3 @@
+export interface Suport{
+   description: String;
+    }
--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {IUser} from './../models/user'
+import {Suport} from './../models/support'
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css']
 })
 export class TemplateComponent implements OnInit {
-  ListUser : IUser[]=[];
-  user: any = {
-    id: this.ListUser.length +1,
-    name: '',
-    email: '',
-    contact: ''
-  };
-  title = "hihi"
+  // ListUser : IUser[]=[];
+  // user: any = {
+  //   id: this.ListUser.length +1,
+  //   name: '',
+  //   email: '',
+  //   contact: ''
+  // };
+  support : Suport = <Suport>{
+    description:''
+  }
   constructor() { }
 
   ngOnInit() {
   }
   addUser(){
-    this.ListUser.push(this.user)
+    // this.ListUser.push(this.user)
 }
 }
